dd2006a4f497af73f0b93c011020185d039a4782
Soichi Hayashi <soichih@gmail.com>
2016-06-22 22:00:03 +0000

    added new service

--- example/register_services.sh
+++ example/register_services.sh
@@ -1,15 +1,16 @@
 
 
-../sca.js service register https://github.com/soichih/sca-service-life
-../sca.js service register https://github.com/soichih/sca-service-noop
-../sca.js service register https://github.com/soichih/sca-service-freesurfer
-../sca.js service register https://github.com/soichih/sca-service-qr
-../sca.js service register https://github.com/soichih/sca-service-hpss
-../sca.js service register https://github.com/soichih/sca-service-osgblast
+#../sca.js service register https://github.com/soichih/sca-service-life
+#../sca.js service register https://github.com/soichih/sca-service-noop
+#../sca.js service register https://github.com/soichih/sca-service-freesurfer
+#../sca.js service register https://github.com/soichih/sca-service-qr
+#../sca.js service register https://github.com/soichih/sca-service-hpss
+#../sca.js service register https://github.com/soichih/sca-service-osgblast
+../sca.js service register https://github.com/soichih/sca-service-fits2png.git
 
-../sca.js service register https://github.com/soichih/sca-product-raw
-../sca.js service register https://github.com/soichih/sca-product-nifti
-../sca.js service register https://github.com/soichih/sca-product-life
-../sca.js service register https://github.com/soichih/sca-product-odi
+#../sca.js service register https://github.com/soichih/sca-product-raw
+#../sca.js service register https://github.com/soichih/sca-product-nifti
+#../sca.js service register https://github.com/soichih/sca-product-life
+#../sca.js service register https://github.com/soichih/sca-product-odi
 
 
